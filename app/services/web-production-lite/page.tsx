5735a8d120d39a166474b2721674b165e9199a60
attgaia <info@attgaia.com>
2025-04-11 19:09:04 +0900

    web-production-lite削除

--- app/services/web-production-lite/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'; // アコーディオンなどクライアントサイドのインタラクションが必要になる可能性があるため
-
-import React from 'react';
-import Image from 'next/image';
-import { Button } from "@/components/ui/button"
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
-import { Check, X, Star, ChevronDown } from 'lucide-react'; // アイコン追加
-import Link from 'next/link';
